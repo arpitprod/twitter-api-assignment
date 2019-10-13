64e01594b21d668b6de0f38b78e0566fa79c24e7
arpitprod <arpitg2@gmail.com>
2019-10-13 21:13:14 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { HistoryProvider } from './context/history';
 
 // Pages
