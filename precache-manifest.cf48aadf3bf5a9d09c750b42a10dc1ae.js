83b504b1c2d197a9757d66dbcd5fd586b0469d84
arpitprod <arpitg2@gmail.com>
2019-10-13 15:22:31 +0530

    Updates

---
+++ precache-manifest.cf48aadf3bf5a9d09c750b42a10dc1ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46ac0e368ebf56584d2fae623c67627",
+    "url": "/twitter-api-assignment/index.html"
+  },
+  {
+    "revision": "f76aa715d57b3605661e",
+    "url": "/twitter-api-assignment/static/css/2.6ef3581e.chunk.css"
+  },
+  {
+    "revision": "fb89bc80eabd8434af1c",
+    "url": "/twitter-api-assignment/static/css/main.254af187.chunk.css"
+  },
+  {
+    "revision": "f76aa715d57b3605661e",
+    "url": "/twitter-api-assignment/static/js/2.fb534354.chunk.js"
+  },
+  {
+    "revision": "fb89bc80eabd8434af1c",
+    "url": "/twitter-api-assignment/static/js/main.d498b449.chunk.js"
+  },
+  {
+    "revision": "20f30b82953d710b0f91",
+    "url": "/twitter-api-assignment/static/js/runtime-main.0722ed31.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/twitter-api-assignment/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/twitter-api-assignment/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/twitter-api-assignment/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/twitter-api-assignment/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/twitter-api-assignment/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
